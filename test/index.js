754b2452e17d59a1de40d6acd676e77b0e18fef9
Kuznetsov Ilia <i.kuznecov@corp.mail.ru>
2015-10-14 18:04:36 +0300

    tested!

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ import 'misc/polyfills';
 import 'misc/dom4';
 import it from 'tape';
 import '../build/importVars';
-/*it('should bind text', (t) => {
+it('should bind text', (t) => {
   t.plan(1);
   var { el, template } = load('../build/simpleText');
   var expectedText = 'some text';
@@ -36,7 +36,7 @@ it('should bind attributes', (t) => {
   };
   template.set(values);
   t.equal(el.getAttribute('class'), 'mark  action--mark action--need-auth test2');
-});*/
+});
 
 
 it('should import vars, vars in attr tmpl', t => {
@@ -54,7 +54,6 @@ it('should import vars, vars in attr tmpl', t => {
   };
   var icon = require('./styles').icon;
   template.set(v);
-  //t.equal(el.getAttribute('attr1'), icon + ' somewhere in the beginning');
   t.equal(el.getAttribute('attr1'), `${icon} ${v.somewhere} in the beginning`);
   t.equal(el.getAttribute('attr2'), `somewhere ${v.at} the end ${icon}`);
   t.equal(el.getAttribute('attr3'), `${v.somewhere} ${icon} in ${v.the} middle`);
@@ -63,15 +62,13 @@ it('should import vars, vars in attr tmpl', t => {
   t.equal(el.getAttribute('attr6'), `is ${v.a} part ${v.of} name-${icon}-in ${v.the} middle`);
   t.equal(el.children[0].getAttribute('attr1'), `inside parent between ${v.two}${icon}${v.vars}`);
 });
-/*it('should bind component', (t) => {
+it('should bind component', (t) => {
   t.plan(1);
-  var {
-    el, template
-  } = load('../build/simpleComponents');
+  var { el, template } = load('../build/simpleComponents');
   template.set('component1', true);
   console.log(template.get('component1'));
   t.equal('I am an component 1!', el.querySelector('h1').textContent);
-});*/
+});
 
 function load(tmlpName) {
   body.innerHTML = '';
